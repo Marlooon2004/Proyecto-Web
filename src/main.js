c9c4da11bd2fd8a7abeb12fefe79989b9a80d7ca
Marlon <marlonrodriguezalvarez890@gmail.com>
2025-11-21 12:11:05 -0500

    proyecto

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PaginaPrincipal from '../views/pagina-principal.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/pagina-principal',
+  },
+  {
+    path: '/pagina-principal',
+    name: 'PaginaPrincipal',
+    component: PaginaPrincipal,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      }
+    }
+    return { top: 0 }
+  },
 })
 
 export default router
