71c440e7cfa1b99a2893ec7eaf449d85939912ce
Marlon <marlonrodriguezalvarez890@gmail.com>
2025-11-22 13:55:43 -0500

    cambio

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 //Pagina principal
 import PaginaPrincipal from '@/views/pagina-principal.vue'
@@ -66,7 +66,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to) {
     if (to.hash) {
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Proyecto-Web/',
+  base : '/Proyecto-Web/',
   plugins: [
     vue(),
     vueDevTools(),
