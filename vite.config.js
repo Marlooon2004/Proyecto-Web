26e474f59a7c1d7982fb21f33f4ec96342539f6d
Marlon <marlonrodriguezalvarez890@gmail.com>
2025-11-22 15:58:44 -0500

    c

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './', // ¡CAMBIAR A RUTA RELATIVA!
+  base: 'Proyecto-Web',
   plugins: [vue()],
   resolve: {
     alias: {
